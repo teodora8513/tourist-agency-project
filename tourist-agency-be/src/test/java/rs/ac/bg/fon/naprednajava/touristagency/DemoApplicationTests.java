f9809ac19fcbd555a934f54280f56b50d5db7ae7
teodora8513 <jevtic.teodora@gmail.com>
2021-05-22 12:54:12 +0200

    first commit

---
+++ tourist-agency-be/src/test/java/rs/ac/bg/fon/naprednajava/touristagency/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.naprednajava.touristagency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
