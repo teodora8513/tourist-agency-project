ad26243e4a700725d7aa8a0a79cd1f597acfb18b
Milena <milenadjj1998@gmail.com>
2021-06-18 16:37:46 +0200

    Adds routes

--- tourist-agency-fe/src/app/app-routing.module.ts
+++ tourist-agency-fe/src/app/app-routing.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import {HeaderComponent} from "./common/components/header/header.component";
+import {BasicLayoutComponent} from "./layouts/basic-layout/basic-layout.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {RegisterComponent} from "./pages/register/register.component";
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: '', component: HeaderComponent, children: [
+      {path: 'home', component: HomeComponent},
+      {path: '', redirectTo: '/home', pathMatch: 'full' }
+    ]
+  },
+  {
+    path: '', component: BasicLayoutComponent, children: [
+      {path: 'login', component: LoginComponent},
+      {path: 'register', component: RegisterComponent}
+    ]
+  },
+  {path: '**', redirectTo: 'home'}
 ];
 
 @NgModule({
