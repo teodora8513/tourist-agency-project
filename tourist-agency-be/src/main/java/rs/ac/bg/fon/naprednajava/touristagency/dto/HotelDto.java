ded8b2bd51a4ce138191825798dc2fc6ef50bf84
Djina <djina.djoric98@gmail.com>
2021-06-24 14:23:25 +0200

    Removed Lombok

--- tourist-agency-be/src/main/java/rs/ac/bg/fon/naprednajava/touristagency/dto/HotelDto.java
+++ tourist-agency-be/src/main/java/rs/ac/bg/fon/naprednajava/touristagency/dto/HotelDto.java
@@ -11,8 +11,6 @@ import rs.ac.bg.fon.naprednajava.touristagency.entity.ReservationEntity;
 import rs.ac.bg.fon.naprednajava.touristagency.entity.RoomEntity;
 import rs.ac.bg.fon.naprednajava.touristagency.enumeration.RoomType;
 
-@AllArgsConstructor @NoArgsConstructor
-@Data
 public class HotelDto implements MyDto{
 
 	private Long id;
@@ -22,5 +20,59 @@ public class HotelDto implements MyDto{
 	private int rating;
 	private DestinationDto destination;
 	
+	public HotelDto() {
+		
+		
+	}
 	
+	public HotelDto(Long id, String name, String address, int rating, DestinationDto destination) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.rating = rating;
+		this.destination = destination;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public int getRating() {
+		return rating;
+	}
+	
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
+	public DestinationDto getDestination() {
+		return destination;
+	}
+	
+	public void setDestination(DestinationDto destination) {
+		this.destination = destination;
+	}
+	
+
 }
--- tourist-agency-be/src/main/java/rs/ac/bg/fon/naprednajava/touristagency/entity/RoomIdentity.java
+++ tourist-agency-be/src/main/java/rs/ac/bg/fon/naprednajava/touristagency/entity/RoomIdentity.java
@@ -8,13 +8,37 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-
-@AllArgsConstructor @NoArgsConstructor
-@Data
 @Embeddable
 public class RoomIdentity implements MyEntity{
 
 	private Long hotel_id;
 	private String room_number;
 	
+	public RoomIdentity() {
+		
+		
+	}
+	
+	public RoomIdentity(Long hotel_id, String room_number) {
+		super();
+		this.hotel_id = hotel_id;
+		this.room_number = room_number;
+	}
+
+	public Long getHotel_id() {
+		return hotel_id;
+	}
+
+	public void setHotel_id(Long hotel_id) {
+		this.hotel_id = hotel_id;
+	}
+
+	public String getRoom_number() {
+		return room_number;
+	}
+
+	public void setRoom_number(String room_number) {
+		this.room_number = room_number;
+	}
+
 }
