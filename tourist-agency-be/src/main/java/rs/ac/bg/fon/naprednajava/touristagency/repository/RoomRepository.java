f7fa7af201a933c7201a26efa341f13f79956e80
Djina <djina.djoric98@gmail.com>
2021-07-08 17:44:14 +0200

    Fixed getRoomByHotelId method

--- tourist-agency-be/src/main/java/rs/ac/bg/fon/naprednajava/touristagency/repository/RoomRepository.java
+++ tourist-agency-be/src/main/java/rs/ac/bg/fon/naprednajava/touristagency/repository/RoomRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import rs.ac.bg.fon.naprednajava.touristagency.entity.HotelEntity;
@@ -12,6 +14,8 @@ import rs.ac.bg.fon.naprednajava.touristagency.entity.RoomIdentity;
 
 @Repository
 public interface RoomRepository extends JpaRepository<RoomEntity, RoomIdentity>{
+	@Query(value = "SELECT room FROM RoomEntity room "
+			+ "WHERE room.hotel = (select hotel from HotelEntity hotel where hotel.id = ?1)")
 	List<RoomEntity> findRoomsByHotelId(Long id);
 }
 
